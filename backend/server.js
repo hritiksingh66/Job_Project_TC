0ca40a8607895e6d7e7c55ad8ab10c73161317f8
Hritik Singh <167676363+hritiksingh66@users.noreply.github.com>
2025-03-05 01:00:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+//MiddleWare
+app.use(cors()); // Enable CORS
+app.use(express.json()); // Parse JSON requests
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); 
 app.use('/api/cards', cardRoutes); // Card routes
 
@@ -21,7 +22,7 @@ mongoose
     console.log("Mongo URI:", process.env.MONGO_URI);
     console.log("Connected to MongoDB");
 
-    // Start server after DB connection is successful
+    // Start server
     app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server is running on port ${PORT}`);
     });
